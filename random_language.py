ebeba227ee74651bdab0c67cf9cac799369cb5c9
Kr3mowka <165086551+Kr3mowka11@users.noreply.github.com>
2025-03-05 22:55:40 +0100

    please

--- random_language.py
+++ random_language.py
@@ -98,13 +98,70 @@ random_lang = random.choice(list(languages.keys()))
 
 code = languages[random_lang]
 
-# Tworzymy zawartość README
 readme_content = f"""# Hi! 👋
 
 ```{random_lang}
 {code}
-```"""
+```
 
-# Zapisz wygenerowaną zawartość do pliku README.md
+## 🔧 Technologies & Tools
+
+**Programming Languages:**
+
+<div align="left">
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" height="40" alt="javascript logo"  />
+  <img width="12" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" height="40" alt="typescript logo"  />
+  <img width="12" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" height="40" alt="python logo"  />
+  <img width="12" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" height="40" alt="c logo"  />
+  <img width="12" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" height="40" alt="cplusplus logo"  />
+  <img width="12" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" height="40" alt="csharp logo"  />
+  <img width="12" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" height="40" alt="java logo"  />
+  <img width="12" />
+</div>
+
+##
+
+**Frontend Development:**
+
+<div align="left">
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" height="40" alt="html5 logo"  />
+  <img width="12" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" height="40" alt="css3 logo"  />
+  <img width="12" />
+  <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwindcss logo" width="40" height="40"/> </a>
+  <img width="12" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" height="40" alt="react logo"  />
+</div>
+
+##
+
+**Currently Learning:**
+
+<div align="left">
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg" height="40" alt="ruby logo"  />
+  <img width="12" />
+</div>
+
+##
+
+## 🏆 GitHub Trophies
+
+<div align="left">
+    <img src="https://github-profile-trophy.vercel.app/?username=kr3mowka11&theme=gruvbox"  />
+</div>
+
+##
+
+<div align="center">
+  <img src="https://github-readme-stats.vercel.app/api?username=Kr3mowka11&hide_title=false&hide_rank=false&show_icons=true&include_all_commits=true&count_private=true&disable_animations=false&theme=dracula&locale=en&hide_border=false&order=1" width="400" height="200"/>
+  <img src="https://github-readme-stats.vercel.app/api/top-langs?username=Kr3mowka11&locale=en&hide_title=false&layout=compact&langs_count=5&theme=dracula&hide_border=false&order=2" width="400" height="200"/>
+</div>"""
+
 with open("README.md", "w") as readme_file:
     readme_file.write(readme_content)
